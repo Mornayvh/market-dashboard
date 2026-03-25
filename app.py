5a7a732c84ebd8e7e44e001fd1606283cef69cf1
Mornay van Heerden <mornay@Mornays-MacBook-Air.local>
2026-03-25 14:51:39 +0200

    Update Home Button

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ st.markdown("""
 
     @media (max-width: 768px) {
         .nav-grid {
-            display: flex; flex-direction: column; margin: 0 auto;
+            display: flex; flex-direction: column; justify-content: center; margin: 0 auto;
         }
     }
 
